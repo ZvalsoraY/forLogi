4af1d085ec1a9e5ab45162ce49dc54320a132b66
ZvalsoraY <yazamail@yandex.ru>
2022-07-07 23:53:35 +0300

    Add project files.

---
+++ Cluster.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace clopeAlg
+{
+    public class Cluster
+    {
+        private int _numberTransactions;
+        private int _square;
+        private double _repulsion;
+        private Dictionary<string, int> _occ = new Dictionary<string, int>();
+
+        public double Repulsion
+        {
+            get => _repulsion;
+            set
+            {
+                _repulsion = value;
+            }
+        }
+
+        public int NumberTransactions
+        {
+            get => _numberTransactions;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(NumberTransactions), "NumberTransactions < 0");
+                }
+                _numberTransactions = value;
+            }
+        }
+        public int Width
+        {
+            get => _occ.Count();
+        }
+        public int Square
+        {
+            get => _square;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(Square), "Square < 0");
+                }
+
+                _square = value;
+            }
+        }
+        public Dictionary<string, int> OCC
+        {
+            get => _occ;
+        }
+
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="rep"> коэффициент отталкивания</param>
+        public Cluster(double repulsion)
+        {
+            _numberTransactions = 0;
+            _repulsion = repulsion;
+        }
+
+        /// <summary>
+        /// Добавление транзакции в кластер
+        /// </summary>
+        /// <param name="transaction">транзакция</param>
+        public void Add(IEnumerable<string> transaction)
+        {
+            _square += transaction.Count();
+            _numberTransactions += 1;
+            foreach (var tranElement in transaction)
+            {
+                if (!_occ.ContainsKey(tranElement))
+                {
+                    _occ.Add(tranElement, 0);/////////////
+                }
+                _occ[tranElement] += 1;
+            }
+        }
+
+        /// <summary>
+        /// Удаление транзакции из кластера
+        /// </summary>
+        /// <param name="transaction">транзакция</param>
+        public void Delete(IEnumerable<string> transaction)
+        {
+            _square -= transaction.Count();
+            _numberTransactions -= 1;
+            foreach (var tranElement in transaction)
+            {
+                _occ[tranElement] -= 1;
+                //if (!IsElementInCluster(tranElement))/////////////
+                //    _width--;
+                if (_occ[tranElement] == 0)
+                {
+                    _occ.Remove(tranElement);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Вычисление прироста при добавлении транзакции
+        /// </summary>
+        /// <param name="transaction">транзакция</param>
+        /// <returns>величина прироста</returns>
+        public double DeltaAdd(IEnumerable<string> transaction)
+        {
+            var S_new = _square + transaction.Count();
+            var W_new = Width;
+            var N_new = _numberTransactions + 1;
+
+            if (_numberTransactions == 0)
+            {
+                W_new = transaction.Distinct().Count();/////
+                return S_new * N_new / Math.Pow(W_new, _repulsion);
+            }
+
+            foreach (var tranElement in transaction)
+            {
+                if (!_occ.ContainsKey(tranElement))
+                {
+                    W_new++;
+                }
+            }
+
+            return (S_new * N_new / Math.Pow(W_new, _repulsion))
+                - (_square * _numberTransactions / Math.Pow(Width, _repulsion));
+        }
+
+        /// <summary>
+        /// Вычисление прироста при удалении транзакции
+        /// </summary>
+        /// <param name="transaction">транзакция</param>
+        /// <returns>величина прироста</returns>
+        public double DeltaSub(IEnumerable<string> transaction)
+        {
+            var S_new = _square - transaction.Count();
+            var W_new = Width;
+            var N_new = _numberTransactions - 1;
+
+            if (_numberTransactions == 1)
+            {
+                return -_square * _numberTransactions / Math.Pow(Width, _repulsion);
+            }
+
+            foreach (var tranElement in transaction)
+            {
+                if (_occ[tranElement] == 1)
+                {
+                    W_new--;
+                }
+            }
+
+            return S_new * N_new / Math.Pow(W_new, _repulsion)
+                - _square * _numberTransactions / Math.Pow(Width, _repulsion);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+namespace clopeAlg
+{
+    internal class Program
+    {
+        public static double repulsion = 2.6;
+        public static string path = @"C:\Users\Master\Documents\C_sharp\Repositories\forLoginom\clopeAlg\agaricus-lepiota.data";
+
+        static void Main()
+        {
+            List<List<string>> transactionsList = new List<List<string>>();
+            foreach (var line in ReadIenumData(path))
+            {
+                //string[] lineString ;
+                List<string> lineString = new List<string>();
+                for (int i = 1; i < line.Count(); i++)
+                {
+                    if (line.ElementAt(i) != "?")
+                    {
+                        lineString.Add(line.ElementAt(i) + i.ToString());
+                    }
+                }
+                transactionsList.Add(lineString);
+            }
+
+            List<Cluster> clusters = new List<Cluster>();
+            Dictionary<int, int> tranClusterDict = new Dictionary<int, int>();// ключ - номер транзакции, значение - номер кластера
+            int selectClusterNum = 0;
+            clusters.Add(new Cluster(repulsion));// добавление пустого кластера
+            Cluster selectCluster = clusters[0];
+
+            foreach (var currentTransaction in transactionsList)//инициализация
+            {
+                double maxProfit = 0;
+                foreach (var currentCluster in clusters)
+                {
+                    if (currentCluster.DeltaAdd(currentTransaction) > maxProfit)
+                    {
+                        maxProfit = currentCluster.DeltaAdd(currentTransaction);
+                        selectCluster = currentCluster;
+                        selectClusterNum = clusters.IndexOf(currentCluster);
+                    }
+                }
+                if (selectCluster.NumberTransactions == 0)
+                {
+                    clusters.Add(new Cluster(repulsion));
+                }
+                selectCluster.Add(currentTransaction);
+                tranClusterDict.Add(transactionsList.IndexOf(currentTransaction), selectClusterNum);//
+            }
+
+            bool moved;
+            do//Итерация
+            {
+                moved = false;
+                for (int i = 0; i < tranClusterDict.Count; i++)
+                {
+                    double maxProfit = 0;
+                    var currentCluster = clusters[tranClusterDict[i]];
+                    var currentTransaction = transactionsList[i];
+                    var delTranFromClasterCost = currentCluster.DeltaSub(currentTransaction);
+                    foreach (var cluster in clusters)
+                    {
+                        if (cluster != currentCluster)
+                        {
+                            if ((cluster.DeltaAdd(currentTransaction) + delTranFromClasterCost) > maxProfit)
+                            {
+                                maxProfit = cluster.DeltaAdd(currentTransaction) + delTranFromClasterCost;
+                                selectCluster = cluster;
+                                selectClusterNum = clusters.IndexOf(cluster);
+                            }
+                        }
+                    }
+                    if (maxProfit > 0)
+                    {
+                        if (selectCluster.NumberTransactions == 0)
+                        {
+                            clusters.Add(new Cluster(repulsion));
+                        }
+                        selectCluster.Add(currentTransaction);
+                        tranClusterDict[i] = selectClusterNum;
+                        currentCluster.Delete(currentTransaction);
+                        moved = true;
+                    }
+                }
+            }
+            while (moved == true);
+            Console.WriteLine("{0,10}   |{1,10}   |{2,10}   |", "CLUSTER", "e", "p");
+            var eCountSum = 0;
+            var pCountSum = 0;
+            for (int i = 0; i < clusters.Count; i++)
+            {
+                if (clusters[i].NumberTransactions != 0)
+                {
+                    var eCount = 0;
+                    var pCount = 0;
+                    foreach (var tranCluster in tranClusterDict.Where(x => x.Value == i))
+                    {
+                        if (ReadIenumData(path).ElementAt(tranCluster.Key).ElementAt(0) == "e")
+                        {
+                            eCount++;
+                        }
+                        else
+                        {
+                            pCount++;
+                        }
+                    }
+                    Console.WriteLine("{0,10}   |{1,10}   |{2,10}   |", i, eCount, pCount);
+                    eCountSum += eCount;
+                    pCountSum += pCount;
+                }
+            }
+            Console.WriteLine("{0,10}   |{1,10}   |{2,10}   |", "Sum", eCountSum, pCountSum);
+        }
+        public static IEnumerable<IEnumerable<string>> ReadIenumData(string filePath)
+        {
+            var lines = File.ReadLines(filePath);
+            return lines.Select(line =>
+            line.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).Select(s => s));
+        }
+    }
+}
